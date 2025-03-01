97684f35713101dc2cfbf5c9773e76e8e5416de4
ritteeka-123 <e0323016@sriher.edu.in>
2025-03-01 14:35:20 +0530

    Create bank_management.java

---
+++ bank_management.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000);
+        account.deposit(500);
+        account.withdraw(200);
+        System.out.println("Current balance: " + account.getBalance());
+    }
+}
